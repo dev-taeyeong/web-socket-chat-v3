77ee46a93e08f1dda8d388023361e71fa81794b4
dev-taeyeong <dev.taeyeong@gmail.com>
2022-08-15 01:56:05 +0900

    Initial commit

---
+++ src/main/java/com/taeyeong/chatv3/Chatv3Application.java
@@ -0,0 +1,13 @@
+package com.taeyeong.chatv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chatv3Application.class, args);
+	}
+
+}
